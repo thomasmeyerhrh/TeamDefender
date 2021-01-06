d69cd9707c3c2a2522f33162ebbb9a95dc82de5b
Thomas MEYER <thomas.meyer3@gmail.com>
2021-01-06 21:47:02 +0100

    Désactivation des scripts de contrôle et les caméras sur les joueurs distants
    Et désactivation / réactivation de la main camera à l'instantiation du joueur

---
+++ Assets/Player/PlayerSetup.cs
@@ -0,0 +1,39 @@
+using UnityEngine;
+using Mirror;
+
+public class PlayerSetup : NetworkBehaviour
+{
+    [SerializeField]
+    Behaviour[] componentsToDisable;
+
+    private Camera _sceneCamera;
+
+    private void Start()
+    {
+        // desactivation des composants si n'est pas le joueur local
+        if (!isLocalPlayer)
+        {
+            foreach(Behaviour behaviour in componentsToDisable)
+            {
+                Behaviour.Destroy(behaviour);
+            }
+        }
+        // notre joueur
+        else
+        {
+            _sceneCamera = Camera.main;
+            if(_sceneCamera != null)
+            {
+                _sceneCamera.gameObject.SetActive(false);
+            }
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (_sceneCamera != null)
+        {
+            _sceneCamera.gameObject.SetActive(true);
+        }
+    }
+}
